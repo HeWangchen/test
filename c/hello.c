12aecc4036b7194595f2ef173ba878920d5b99e8
HeWangchen <hewangchen0727@gmail.com>
2012-08-05 13:06:17 +0800

    initial commit

---
+++ c/hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+	printf("hello git~~!");
+	return 0;
+}
---
+++ java/hello.java
@@ -0,0 +1,9 @@
+import java.utils.*;
+
+public class hello
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello git~~!");
+	}
+}
